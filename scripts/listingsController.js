a50b38266e982a559a29e564dd638110e4cab845
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-15 15:39:24 +0900

    Create factory for listings

--- scripts/listingsController.js
+++ scripts/listingsController.js
@@ -1,25 +1,6 @@
 angular
   .module("listingsApp")
-  .controller("listingsController", function ($scope) {
+  .controller("listingsController", function ($scope, listingsFactory) {
     $scope.hello = "Hello World!";
-    $scope.listings = [
-      {
-        type: "Condo",
-        price: 22000,
-        address: "213 Grove Street",
-        description: "Excellent place, really nice view!",
-      },
-      {
-        type: "House",
-        price: 410500,
-        address: "7823 Winding Way",
-        description: "Beautiful home with lots of space for a large family",
-      },
-      {
-        type: "Duplex",
-        price: 395000,
-        address: "834 River Lane",
-        description: "Grear neighbourhood and lots of nice green space.",
-      },
-    ];
+    $scope.listings = listingsFactory.getListings();
   });
